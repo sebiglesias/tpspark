b1d12125e45e60c9971f59064ad60079514efa84
Sebastian Iglesias <choalejo@hotmail.com>
2017-11-22 17:59:47 -0300

    Finished b)

--- src/main/scala/InvertedIndex.scala
+++ src/main/scala/InvertedIndex.scala
@@ -3,11 +3,22 @@
   */
 
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 
 object InvertedIndex {
   def main(args: Array[String]): Unit = {
-    val sc = new SparkContext("local", "InvertedIndexApp")
+    val sc = new SparkContext("local[*]", "InvertedIndexApp")
     val textFile = sc.wholeTextFiles("/Users/sebastian.iglesias/projects/faculty/Distribuidos/tpspark/src/main/scala/data/books/*")
+    textFile.flatMap{
+      case (name, content) =>
+        val words = content.toLowerCase.split("""\W+""")
+        words.map(word => (word,List(name):List[String]))
+    }.reduceByKey{
+      case (list1, list2) =>
+        var result:List[String] = list1
+        list2.foreach(filename =>
+        if(!list1.contains(filename)) result = List(filename):::result)
+        result
+    }.saveAsTextFile("/Users/sebastian.iglesias/projects/faculty/Distribuidos/tpspark/src/main/scala/output")
+    sc.stop()
   }
 }
