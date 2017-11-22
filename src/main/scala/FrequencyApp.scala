bca10ae0a1fdaa211bf0e6c8ca29697a3744a4db
Sebastian Iglesias <choalejo@hotmail.com>
2017-11-22 18:09:19 -0300

    Cleaning code

--- src/main/scala/FrequencyApp.scala
+++ src/main/scala/FrequencyApp.scala
@@ -8,7 +8,7 @@ object FrequencyApp {
   def main(args: Array[String]): Unit = {
     val sc = new SparkContext("local", "FrequencyApp")
     val textFile = sc.textFile("/Users/sebastian.iglesias/projects/faculty/Distribuidos/tpspark/src/main/scala/data/books/*")
-    val counts = textFile.flatMap(line => line.split("\\W+"))
+    val counts = textFile.flatMap(line => line.split("""\W+"""))
       .map(word => (word.toLowerCase, 1))
       .reduceByKey(_ + _)
       .map(touple => (touple._2,touple._1))
